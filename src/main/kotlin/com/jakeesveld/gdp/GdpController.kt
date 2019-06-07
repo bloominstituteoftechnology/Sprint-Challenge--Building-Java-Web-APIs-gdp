78aeab4dceffd6effabc7c4ae122e92469299aaa
Jake Esveld <thejakeesveld@gmail.com>
2019-06-07 08:44:05 -0700

    Add Logging for endpoints

--- src/main/kotlin/com/jakeesveld/gdp/GdpController.kt
+++ src/main/kotlin/com/jakeesveld/gdp/GdpController.kt
@@ -1,5 +1,7 @@
 package com.jakeesveld.gdp
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,23 +13,29 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/gdp"])
 class GdpController{
 
+    val logger: Logger = LoggerFactory.getLogger(GdpController::class.java)
+
     @GetMapping(value = ["/names"])
     fun getGdpNamesAlphabetized(): ResponseEntity<Any>{
+        logger.info("/names accessed")
         return ResponseEntity(GdpApplication.gdpList.sortBy { x -> x.name }, HttpStatus.OK)
     }
 
     @GetMapping(value = ["/economy"])
     fun getGdpByEconomy(): ResponseEntity<Any>{
+        logger.info("/economy accessed")
         return ResponseEntity(GdpApplication.gdpList.sortBy { x -> x.gdp }, HttpStatus.OK)
     }
 
     @GetMapping(value = ["/country/{id}"])
     fun getGdpById(@PathVariable id: Long):ResponseEntity<Any>{
+        logger.info("/country/$id accessed")
         return ResponseEntity(GdpApplication.gdpList.filter { x -> x.id == id }, HttpStatus.OK)
     }
 
     @GetMapping(value = ["/country/stats/median"])
     fun getMedianGdp(): ResponseEntity<Any>{
+        logger.info("/country/stats/median accessed")
         val tempList = GdpApplication.gdpList.sortedBy { x -> x.gdp }
         return ResponseEntity(tempList[tempList.size / 2], HttpStatus.OK)
     }
