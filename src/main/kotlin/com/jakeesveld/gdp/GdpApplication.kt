c101fec1c4b6c7f00a163cfe496e254b12498944
Jake Esveld <thejakeesveld@gmail.com>
2019-06-07 08:04:52 -0700

    Initial commit

---
+++ src/main/kotlin/com/jakeesveld/gdp/GdpApplication.kt
@@ -0,0 +1,11 @@
+package com.jakeesveld.gdp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GdpApplication
+
+fun main(args: Array<String>) {
+    runApplication<GdpApplication>(*args)
+}
