7b133cde492995a302e1c316d6c22ce324a9e3bf
Shoonsk8 <shoon@att.net>
2019-06-14 11:14:29 -0400

    initial dogs

---
+++ java/src/main/java/com/lambdaschool/dogs/CheckDog.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.dogs;
+
+
+import com.lambdaschool.dogs.model.Dog;
+
+public interface CheckDog
+{
+    boolean test(Dog d);
+}
---
+++ java/src/main/java/com/lambdaschool/dogs/DogsinitialApplication.java
@@ -0,0 +1,27 @@
+package com.lambdaschool.dogs;
+
+import com.lambdaschool.dogs.model.DogList;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableScheduling
+@EnableWebMvc
+@SpringBootApplication
+public class DogsinitialApplication
+{
+
+    public static DogList ourDogList;
+    public static void main(String[] args)
+    {
+        ourDogList = new DogList();
+
+        ApplicationContext ctx = SpringApplication.run(DogsinitialApplication.class, args);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
+}
+
